bdd8db1ffe2ca6c42b6f08df88d0820a5d343d52
meerabenson <meeratucson@yahoo.com>
2017-02-08 09:51:44 -0700

    Update server.js

--- todoExample/server.js
+++ todoExample/server.js
@@ -10,47 +10,19 @@ app.get("/", function (req, res) {
       res.redirect("/index.html");
 });
 
+
 var todoList = [];
 
+
 var addCallback = function (req, res) {
-   console.log(req.query);
-   todoList.push(req.query);
-    res.send("1"); // send response body
+    console.log(req.query);
+    todoList.push(req.query);
+
 }
+    res.send("Todo added!");
 
 
-var subCallback = function (req, res) {
-    var a = parseFloat(req.query.a);
-    var b = parseFloat(req.query.b);
-    var result  = a - b;
-
-    res.send(result.toString()); // send response body
-}
-
-
-var multCallback = function (req, res) {
-    var a = parseFloat(req.query.a);
-    var b = parseFloat(req.query.b);
-    var result  = a * b;
-
-    res.send(result.toString()); // send response body
-}
-
-
-var divCallback = function (req, res) {
-    var a = parseFloat(req.query.a);
-    var b = parseFloat(req.query.b);
-    var result  = a / b;
-
-    res.send(result.toString()); // send response body
-}
-
-app.get("/add", addCallback);
-app.get("/sub", subCallback);
-app.get("/mult", multCallback);
-app.get("/div", divCallback);
-
-
+app.get("/addTodo", addCallback);
 
 
 
